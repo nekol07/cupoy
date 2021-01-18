258b8c068dd443ecb4bff8cce14b798dbc8876a1
Lance <nava@ms21.url.com.tw>
2021-01-18 23:54:27 +0800

    調整陣列轉成維度，順序按列填充 (order="F")

--- numpy_homework_02.py
+++ numpy_homework_02.py
@@ -5,9 +5,9 @@ import numpy as np
 array1 = np.array(range(30))
 print(f"Origin Array = {array1}\n")
 
-array_5x6 = array1.reshape((5, 6))
+array_5x6 = array1.reshape(5, 6, order="F")
 print(f"New Array 5x6 = {array_5x6}\n")
 
-index = np.where(array1%6 == 1)
+index = np.where(array_5x6%6 == 1)
 print(f"Index: {index}")
 
