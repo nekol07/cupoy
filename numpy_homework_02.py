28a5d68667602262f8dcdb01d6ad1a3747f08041
Lance <nava@ms21.url.com.tw>
2021-01-18 23:40:01 +0800

    陣列維度轉換，並且會擷取需要資料
    1.將下列陣列(array1)，轉成維度為(5X6)的array，順序按列填充。(hint:order="F")
    2.呈上題的array，找出被6除餘1的數的索引

---
+++ numpy_homework_02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+array1 = np.array(range(30))
+print(f"Origin Array = {array1}\n")
+
+array_5x6 = array1.reshape((5, 6))
+print(f"New Array 5x6 = {array_5x6}\n")
+
+index = np.where(array1%6 == 1)
+print(f"Index: {index}")
+
